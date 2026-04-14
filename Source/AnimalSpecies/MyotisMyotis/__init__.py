77d8bf56ad36560ce8842417f6fae5e503cc0440
Valeriy Shapkin <valllshapkin@gmail.com>
2026-04-14 18:44:39 +0300

    auto

--- Source/AnimalSpecies/MyotisMyotis/__init__.py
+++ Source/AnimalSpecies/MyotisMyotis/__init__.py
@@ -2,4 +2,4 @@ from BatSpec.Logic.Functions import SpecFunc
 from BatSpec.Logic.SpecWorker import centrateCorpMass
 from pathlib import Path
 
-TEST_ETALON = centrateCorpMass(SpecFunc.load(str(Path(__file__).parent / "Etalons" / "A004057_DGWBDAAKKD.093.npz")))
+TEST_ETALON = SpecFunc.load(str(Path(__file__).parent / "Etalons" / "A004057_DGWBDAAKKD.093.npz"))
