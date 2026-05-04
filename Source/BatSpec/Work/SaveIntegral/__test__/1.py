94edb58d49b43b1ba84ca24c31ce3a2e475e94de
Valeriy Shapkin <valllshapkin@gmail.com>
2026-05-04 06:32:38 +0300

    auto

--- Source/BatSpec/Work/SaveIntegral/__test__/1.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-ScriptDir = Path(__file__).parent
-from BatSpec.Work.Record import loadRecord
-from BatSpec.Work.SaveIntegral import SaveIntegralEnergy
-
-record = loadRecord(ScriptDir / "MYODAS_20230624_004924.wav")
-print(SaveIntegralEnergy(record))
